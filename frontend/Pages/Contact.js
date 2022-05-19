4fe47cad590c8d0e8507da79c03cfc3b1d77915d
codyrzipp <55764584+codyrzipp@users.noreply.github.com>
2022-05-18 22:45:38 -0700

    removed commented code

--- frontend/Pages/Contact.js
+++ frontend/Pages/Contact.js
@@ -2,56 +2,6 @@ import React from 'react';
 import { ImageBackground, StyleSheet, Text, TextInput, View, Button } from 'react-native';
 import { useForm, Controller } from "react-hook-form";
 
-// export default function App() {
-    // const { control, handleSubmit, formState: { errors } } = useForm({
-    //     defaultValues: {
-    //         fistName: '',
-    //         lastName: ''
-    //     }
-    // });
-    // const onSubmit = data => console.log(data);
-
-//     return (
-//         <View>
-//             <Controller 
-//             control={control}
-//             rules={{
-//                 required: true,
-//             }}
-//             render={({ field: { onChange, onBlur, value } }) => (
-//                 <TextInput
-//                 style={styles.input}
-//                 onBlur={onBlur}
-//                 onChangeText={onChange}
-//                 value={value}
-//                 />
-//             )}
-//             name="First Name"
-//             />
-
-//             {errors.firstName && <Text>This is required.</Text>}
-
-//             <Controller
-//                 control={control}
-//                 rules={{
-//                     maxLength: 100,
-//                 }}
-//                 render={({ field: { onChange, onBlur, value } }) => (
-//                     <TextInput
-//                         style={styles.input}
-//                         onBlur={onBlur}
-//                         onChange={onChange}
-//                         value={value}
-//                     />
-//                 )}
-//                 name="lastName"
-//                 />
-
-//                 <Button title="Submit" onPress={handleSubmit(onSubmit)} />
-//         </View>
-//     );
-// }
-
 const ContactScreen = () => {
     const { control, handleSubmit, formState: { errors } } = useForm({
         defaultValues: {
